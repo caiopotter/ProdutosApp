edfe981984513730e4deb2a077e0ab17c7107c97
Caio Potter <limapotter@gmail.com>
2025-05-09 16:27:54 -0300

    feat: Criação do contexto do EF e configurações de ioc

---
+++ ProdutosApp.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutosApp.Infra.Data.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApp.Infra.Data.Contexts
+{
+    /// <summary>
+    /// Classe de contexto do Entity Framework Core para a aplicação ProdutosApp.
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            
+        }
+
+        /// <summary>
+        /// Método para adicionar as classes de mapeamento feitas no projeto
+        /// 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProdutoMap());
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+        }
+    }
+}
